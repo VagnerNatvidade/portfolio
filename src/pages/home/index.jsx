bb7ad1e89d0c98808a45e50f92aaee7e20b71a1a
VagnerNatvidade <vagnerflorindojunior@gmail.com>
2024-07-24 00:11:15 -0300

    ajusta main na página home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,6 +5,25 @@ export function Home() {
   return (
     <Container>
       <Header />
+      <div className="main">
+        <h1>
+          Vagner Natividades<span>.</span>
+        </h1>
+        <p>
+          I'm just trying to use all my studies and love for programming to help
+          other people.
+        </p>
+        <div>
+          <p>
+            My greatest skills are in JavaScript and React.JS, despite studying
+            Node.Js
+          </p>
+          <p>
+            Developer Status: anxious to start the journey as a junior developer
+            in a company.
+          </p>
+        </div>
+      </div>
     </Container>
   );
 }
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -1,6 +1,40 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  display: grid;
+  grid-template-rows: max-content 1fr 1fr;
   height: 100vh;
   background: ${({ theme }) => theme.COLORS.BLUE_100};
+
+  .main {
+    min-width: 360px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 24px 40px;
+    color: ${({ theme }) => theme.COLORS.LIGHT_100};
+
+    h1 {
+      width: 180px;
+      margin-bottom: 16px;
+      color: ${({ theme }) => theme.COLORS.YELLOW_100};
+
+      span {
+        color: ${({ theme }) => theme.COLORS.LIGHT_100};
+      }
+    }
+
+    > p {
+      margin-bottom: auto;
+    }
+
+    > div {
+      display: flex;
+      justify-content: space-between;
+      p {
+        width: 46%;
+        font-size: 9px;
+      }
+    }
+  }
 `;
